131050feb66cf94354cc2e0cdb577ddd941a6af5
Sakeena Usan bee .M <sakeena1112005@gmail.com>
2025-09-30 21:31:05 +0530

    datatype

--- Demo.java
+++ Demo.java
@@ -1,5 +1,8 @@
 public class Demo {
     public static void main(String []args){
-        System.out.println("BISMILLAH");
+        int a=2005;
+        String b="sakeena";
+        System.out.println(a);
+        System.out.println(b);
     }
 }
