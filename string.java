bef0cd36e3c6aed9cf93e0ef3c7d743acd1f23ac
Sakeena Usan bee .M <sakeena1112005@gmail.com>
2025-10-01 22:06:31 +0530

    string

---
+++ string.java
@@ -0,0 +1,8 @@
+public class string {
+    public static void main(String[] args) {
+        String a1=new String("sakeena");
+        String a2=new String("sakeena");
+        System.out.println(a1.equals(a2));
+        System.out.println(a1==a2);
+    }
+}
