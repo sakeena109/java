ad63a91c8bcc91b9a5facb51011e81bec189e8d6
Sakeena Usan bee .M <sakeena1112005@gmail.com>
2025-10-01 17:28:08 +0530

    scanner

--- qn2.java
+++ qn2.java
@@ -1,7 +1,10 @@
+import java.util.Scanner;
+
 public class qn2 {
     public static void main(String[] args) {
-        int a=40;
-        int b=50;
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
         if(a<=b){
             System.out.println("b is greater");
         }
