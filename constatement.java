639df5549dcb2b9f6672a49f3e1db868710086da
Sakeena Usan bee .M <sakeena1112005@gmail.com>
2025-10-01 12:18:51 +0530

    conditional statement

---
+++ constatement.java
@@ -0,0 +1,12 @@
+public class constatement {
+    public static void main(String[] args) {
+        boolean rain=true;
+        if(rain){
+            System.out.println("take an umbrella");
+        }
+        else{
+            System.out.println("no need to take umbrella");
+        }
+    }
+    
+}
