66c2f29d2e893b7752370fc539b1e7b029685146
Sakeena Usan bee .M <sakeena1112005@gmail.com>
2025-10-01 11:30:03 +0530

    scanner

--- qn1.java
+++ qn1.java
@@ -2,13 +2,12 @@ import java.util.Scanner;
 public class qn1 {
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
-        int d=a+b+c;
-        int e=a*a;
-        System.out.println(d);
-        System.out.println(e);
-        System.out.println(e/d);
+        String name=sc.nextLine();
+        int score=sc.nextInt();
+        sc.nextLine();
+        String department=sc.nextLine();
+        System.out.println("My name is:"+name);
+        System.out.println("My score is:"+score);
+        System.out.println("My department is:"+department);
     }
 }
