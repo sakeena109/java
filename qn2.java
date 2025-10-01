39b9a8cbdc5d84872153286bef0cecec9ca11636
Sakeena Usan bee .M <sakeena1112005@gmail.com>
2025-10-01 17:20:06 +0530

    lesser

--- qn2.java
+++ qn2.java
@@ -2,11 +2,11 @@ public class qn2 {
     public static void main(String[] args) {
         int a=40;
         int b=50;
-        if(a==b){
-            System.out.println("true");
+        if(a<=b){
+            System.out.println("b is greater");
         }
         else{
-            System.out.println("false");
+            System.out.println("a is greater");
         }
     }
 }
