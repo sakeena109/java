0ea38aa92662ddea0675fff9c2470e88d507cdda
Sakeena Usan bee .M <sakeena1112005@gmail.com>
2025-10-01 17:13:45 +0530

    practice 2

---
+++ qn2.java
@@ -0,0 +1,12 @@
+public class qn2 {
+    public static void main(String[] args) {
+        int a=40;
+        int b=50;
+        if(a==b){
+            System.out.println("true");
+        }
+        else{
+            System.out.println("false");
+        }
+    }
+}
