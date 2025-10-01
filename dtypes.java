07a69585bd9c7874c97b2e240ed32bd7878f3914
Sakeena Usan bee .M <sakeena1112005@gmail.com>
2025-10-01 12:01:25 +0530

    datatypes

---
+++ dtypes.java
@@ -0,0 +1,16 @@
+public class dtypes {
+    public static void main(String[] args) {
+        int a=10;
+        float b=10.123456f;
+        double c=28.9;
+        short d=10;
+        long e=99967;
+        String f="sakeena";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
