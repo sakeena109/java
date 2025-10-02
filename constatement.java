deab656c44e69d1cadd07d0ef361c31492fee7bc
Sakeena Usan bee .M <sakeena1112005@gmail.com>
2025-10-02 19:49:09 +0530

    divisible

--- constatement.java
+++ constatement.java
@@ -1,9 +1,15 @@
 public class constatement {
     public static void main(String[] args) {
-        for(int i=0;i<=10;i++){
-            i=i+1;
-            System.out.println(i);
+        for(int i=1;i<=100;i++){
+            if(i%3==0&&i%5==0)
+            {
+                System.out.println("divisible by 3 and 5");
+            }
+            else{
+                System.out.println("not divisible by 3 and 5");
+            }
         }
+        
     }
 }
 
