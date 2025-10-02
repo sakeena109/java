7155de288d4071f093d979159684e6e4e30cdeb6
Sakeena Usan bee .M <sakeena1112005@gmail.com>
2025-10-02 13:54:32 +0530

    ternary

---
+++ ternary.java
@@ -0,0 +1,7 @@
+public class ternary {
+    public static void main(String[] args) {
+        boolean rain=true;
+        String result=rain?"take an umbrella":"enjoy the sunlight";
+        System.out.println(result);
+    } 
+}
