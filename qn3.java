c9bca492df94f9056c0453ac9e806689d604ba35
Sakeena Usan bee .M <sakeena1112005@gmail.com>
2025-10-02 12:51:53 +0530

    else if

--- qn3.java
+++ qn3.java
@@ -1,11 +1,17 @@
 public class qn3 {
     public static void main(String[] args){
-        int num=20;
-        if(num%3==0&&num%5==0){
-            System.out.println("num is divisible by 3");
+        int score=90;
+        if(score>=90){
+            System.out.println("you can get macbook");
+        }
+        else if(score>=60){
+            System.out.println("you can get iphone ");
+        }
+        else if(score>=35){
+            System.out.println("you can get video game");
         }
         else{
-            System.out.println("num is not divisible by 3");
+            System.err.println("you can get nothing");
         }
     }
 }
