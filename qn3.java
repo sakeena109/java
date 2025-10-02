096459f749d07dd7f9d35d154e141eb08738245e
Sakeena Usan bee .M <sakeena1112005@gmail.com>
2025-10-02 12:13:56 +0530

    divisible by 3

---
+++ qn3.java
@@ -0,0 +1,11 @@
+public class qn3 {
+    public static void main(String[] args){
+        int num=15;
+        if(num%3==0){
+            System.out.println("num is divisible by 3");
+        }
+        else{
+            System.out.println("num is not divisible by 3");
+        }
+    }
+}
