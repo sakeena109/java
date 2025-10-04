74aa525fb9b202a1c4625b97304fca7cb3bf5194
Sakeena Usan bee .M <sakeena1112005@gmail.com>
2025-10-04 21:30:44 +0530

    new syntax for array

--- array.java
+++ array.java
@@ -1,11 +1,14 @@
 public class array {
     public static void main(String[] args) {
-        double[] temp={22.4,12.6,23.6,26.9};
-        double max=temp[0];
-        for (int i=1;i<temp.length;i++) {
-            if(temp[i]>max)
-            max=temp[i];
-        }
-        System.out.println(max);
+        String name="sakeena";
+        String namee="suhaina";
+        String nameee="maidha";
+        String[] list=new String[3];
+        list[1]="suhaina";
+        list[0]="maidha";
+        list[2]="sakeena";
+        System.out.println(list[0]);
+        System.out.println(list[1]);
+        System.out.println(list[2]);
     }
 }
