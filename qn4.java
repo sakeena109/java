4db9de1c2de834cb319c8e2defc9fc704ebd0344
Sakeena Usan bee .M <sakeena1112005@gmail.com>
2025-10-04 21:49:33 +0530

    string

---
+++ qn4.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class qn4 {
+    public static void main(String[] args) {
+        int[] mark=new int[5];
+        Scanner sc=new Scanner(System.in);
+        mark[0]= sc.nextInt();
+        mark[1]= sc.nextInt();
+        mark[2]= sc.nextInt();
+        mark[3]= sc.nextInt();
+        mark[4]= sc.nextInt();
+        System.out.println(mark[1]);
+    }
+}
