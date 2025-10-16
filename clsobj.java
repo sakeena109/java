ca86905ba7d1175ad04f88bb268344a679705fcf
sakeena usan bee M <sakeena1112005@gmail.com>
2025-10-16 20:21:19 +0530

    abstraction

---
+++ clsobj.java
@@ -0,0 +1,20 @@
+public class clsobj {
+    int coffee=20;
+    int tea=15;
+    int milk=10;
+
+    public static void main(String[] args){
+
+        clsobj server1=new clsobj();
+        System.out.println(server1.coffee);
+
+        clsobj server2=new clsobj();
+        System.out.println(server2.tea);
+        
+
+        clsobj server3=new clsobj();
+        System.out.println(server3.milk);
+
+
+    }
+}
