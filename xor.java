9b540899435d8fa33881728e426f722971564e0f
sakeena usan bee M <sakeena1112005@gmail.com>
2026-02-09 12:00:37 +0530

    xor

--- xor.java
+++ xor.java
@@ -1,12 +1,8 @@
 public class xor {
-    public static void main(String[] args){
-        int a=6;
-        int b=7;
-        int c=8;
-        int result=a^b^c;
-        int result2=c^b^a;
-
-        System.out.println(result);
-        System.out.println(result2);
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int c = (int) (a / Math.pow(2, b));
+        System.out.println(c);
     }
 }
