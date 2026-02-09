c1cb29f635bb9acb7b1a9099fac00d3a0a9653f2
sakeena usan bee M <sakeena1112005@gmail.com>
2026-02-09 11:21:44 +0530

    xor

---
+++ xor.java
@@ -0,0 +1,12 @@
+public class xor {
+    public static void main(String[] args){
+        int a=6;
+        int b=7;
+        int c=8;
+        int result=a^b^c;
+        int result2=c^b^a;
+
+        System.out.println(result);
+        System.out.println(result2);
+    }
+}
